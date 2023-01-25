a86a0e0ab6f46f86e62265d5b187b88752521b82
sporty786 <michelle.e.tracy@gmail.com>
2023-01-25 21:50:44 +0000

    Wk 5 Day 3 Lecture notes Maven/Javalin

---
+++ javalin-demo/src/main/java/com/revature/App.java
@@ -0,0 +1,20 @@
+package com.revature;
+
+import io.javalin.Javalin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Javalin app = Javalin.create(/*config*/);
+        app.get("/", ctx -> ctx.result("Hello World/n"));
+        app.start(7071);
+    }
+}
+
+// Open new terminal (can split terminals)
+// terminal commands: $ curl localhost: 7071
